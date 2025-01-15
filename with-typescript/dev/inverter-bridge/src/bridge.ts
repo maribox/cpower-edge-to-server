4dcb05e35f9df760650a87a059d0f526ac612cfe
Marius Bosler <70728036+maribox@users.noreply.github.com>
2025-01-15 05:38:32 +0100

    added with-typescript

---
+++ with-typescript/dev/inverter-bridge/src/bridge.ts
@@ -0,0 +1,61 @@
+import createClient from "openapi-fetch"
+import type { paths, components } from "../schema"
+import { z } from 'zod';
+
+const envSchema = z.object({
+  REMOTE_SERVER_URL: z.union([z.string().ip(), z.string().url()]),
+  USER_ID: z.string().uuid(),
+  INVERTER_ADDRESS: z.union([z.string().ip(), z.string().url()]),
+  INTERVAL: z.string().pipe(z.coerce.number()),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error('Invalid environment variables:', parsedEnv.error.format());
+  process.exit(1);
+}
+
+const { REMOTE_SERVER_URL, USER_ID, INVERTER_ADDRESS, INTERVAL } = parsedEnv.data;
+
+const client = createClient<paths>({ baseUrl: `http://${INVERTER_ADDRESS}` });
+
+async function fetchAndForward() {
+  try {
+    const {
+      data, 
+      error,
+    } = await client.GET("/solar_api/v1/GetMeterRealtimeData.cgi", {
+      params: {
+        "query": { "Scope": "System"},
+      },
+    });
+    if (error) {
+      throw new Error(`Failed to fetch from ${INVERTER_ADDRESS}: ${JSON.stringify(error)}`);
+    }
+    
+    console.log("Fetched data:", data.Body.Data);
+    let powerInWatts = Number(data.Body.Data["0"]!!.PowerReal_P_Sum)
+    
+    let body = JSON.stringify({
+      "power_in_w" : powerInWatts,
+      "user_id": USER_ID,
+    })
+
+    const postResponse = await fetch(REMOTE_SERVER_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    });
+
+    if (!postResponse.ok) {
+      throw new Error(`Failed to forward data: ${postResponse.statusText}`);
+    }
+
+    console.log("Data forwarded successfully.");
+  } catch (error) {
+    console.error("Error fetching or forwarding data:", error);
+  }
+}
+setInterval(fetchAndForward, INTERVAL);
+fetchAndForward();
